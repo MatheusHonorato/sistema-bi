8a071b00279c4d55b6a563f61a03e271d142edeb
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-03-29 10:35:35 -0300

    up

--- app/resources/views/livewire/dashboard.blade.php
+++ app/resources/views/livewire/dashboard.blade.php
@@ -102,7 +102,17 @@
                                 </div>
                             </div>
 
-                            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Exportar</button>
+                            <div class="text-2xl">
+                                <button id="dropdownExport" data-dropdown-toggle="export" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Exportar<svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+
+                                <div id="export" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+                                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
+                                        <li class="flex flex-row justify-between px-1 cursor-pointer">Telefone</li>
+                                        <li class="flex flex-row justify-between px-1 cursor-pointer">SMS</li>
+                                        <li class="flex flex-row justify-between px-1 cursor-pointer">Mala direta</li>
+                                    </ul>
+                                </div>
+                            </div>
 
                         </div>
 
