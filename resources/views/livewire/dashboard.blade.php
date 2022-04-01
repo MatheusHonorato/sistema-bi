b5692edf2cdc8db8022f94e65aa093443684be1c
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-04-01 00:56:15 -0300

    up

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -6,12 +6,10 @@
                 <div class="bg-white overflow-hidden">
                     <div class="sm:px-10 py-2 bg-white border-b border-gray-200 columns-1 sm:columns-8">
                         <div class="w-full">
-                            <div class="w-full flex items-center">
-                                Simulação
-                            </div>
-                            <div class="w-full mr-2 font-medium">
-                                Total: {{ $amount }}
-                            </div>
+                            Simulação
+                        </div>
+                        <div class="w-full mr-2 font-medium">
+                            Total: {{ $amount }}
                         </div>
                         <div class="w-full text-2xl text-center">
                             <button id="dropdownDefault" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"><i class="fa-solid fa-filter mr-2"></i> Estados<svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
