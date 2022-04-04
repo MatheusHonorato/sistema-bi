8d6460036b4accba6dd3a07e1016519f0fd516a7
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-04-04 15:58:33 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,23 @@ use App\Http\Livewire\Clients;
 use App\Http\Livewire\Dashboard;
 use App\Http\Livewire\Phone;
 
-//Route::get('clients', Clients::class)->name('clients');
+Route::get('clients', Clients::class)->name('clients');
 
 Route::get('phones/{id}', Phone::class)->name('phones.show');
 
 Route::get('tabulacao-de-dados', Dashboard::class)->name('dashboard');
 
-Route::view('/', '/tabulacao-de-dados')->name('welcome');
+Route::view('/', 'quem-somos')->name('welcome');
+
+Route::redirect('/', '/tabulacao-de-dados', 301);
+
+
 Route::view('/quem-somos', 'quem-somos')->name('quem-somos');
 Route::view('/pagamento-online', 'comprar')->name('comprar');
 Route::view('/contato', 'contato')->name('contato');
 Route::view('/nossos-servicos', 'nossos-servicos')->name('nossos-servicos');
 
-//Route::post('send-contact-mail', [ContactMailController::class, 'index'])->name('contact.mail');
+Route::post('send-contact-mail', [ContactMailController::class, 'index'])->name('contact.mail');
 
 Route::redirect('/register', '/tabulacao-de-dados', 301);
 
