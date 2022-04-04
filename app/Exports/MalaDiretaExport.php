6b85420d53753f9e50e490446265fc521de5aad6
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-04-04 17:26:18 -0300

    up

--- app/Exports/MalaDiretaExport.php
+++ app/Exports/MalaDiretaExport.php
@@ -22,32 +22,6 @@ class MalaDiretaExport implements FromCollection
         $this->to = $to;
     }
 
-    private function telefone($n)
-    {
-        $tam = strlen(preg_replace("/[^0-9]/", "", $n));
-
-        if ($tam == 13) {
-            // COM CÓDIGO DE ÁREA NACIONAL E DO PAIS e 9 dígitos
-            return "+".substr($n, 0, $tam-11)." (".substr($n, $tam-11, 2).") ".substr($n, $tam-9, 5)."-".substr($n, -4);
-        }
-        if ($tam == 12) {
-            // COM CÓDIGO DE ÁREA NACIONAL E DO PAIS
-            return "+".substr($n, 0, $tam-10)." (".substr($n, $tam-10, 2).") ".substr($n, $tam-8, 4)."-".substr($n, -4);
-        }
-        if ($tam == 11) {
-            // COM CÓDIGO DE ÁREA NACIONAL e 9 dígitos
-            return " (".substr($n, 0, 2).") ".substr($n, 2, 5)."-".substr($n, 7, 11);
-        }
-        if ($tam == 10) {
-            // COM CÓDIGO DE ÁREA NACIONAL
-            return " (".substr($n, 0, 2).") ".substr($n, 2, 4)."-".substr($n, 6, 10);
-        }
-        if ($tam <= 9) {
-            // SEM CÓDIGO DE ÁREA
-            return substr($n, 0, $tam-4)."-".substr($n, -4);
-        }
-    }
-
     public function collection()
     {
 
@@ -60,7 +34,6 @@ class MalaDiretaExport implements FromCollection
         $collection = collect();
 
         foreach ($clients as $client) {
-            $phone_datas = Phone::where('client_id', $client->id)->first();
 
             $tratamento = '`A';
 
@@ -84,7 +57,7 @@ class MalaDiretaExport implements FromCollection
                 'city' => $client->city->name,
                 'state' => $client->city->state->name,
                 'email' => $client->city->email,
-                'phone' => $this->telefone($phone_datas->ddd . $phone_datas->phone),
+                'phone' => '',
                 'fax' => '',
                 'cep_caixa_postal' => '',
                 'cep' => $client->cep
