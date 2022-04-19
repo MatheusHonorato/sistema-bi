bffb571e21c6d771a7b05fefd818cbc26ad0e907
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-04-19 13:04:00 -0300

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                         <img src="banner.jpg" width="600px" id="banner">
                         <a href="{{ route('dashboard') }}">
                             <div class="px-10 md:px-0 flex flex-col sm:mt-20 mt-36">
-                                <h1 class="mt-10 mb-5 text-center">Bem vindo(a) á</h1>
+                                <h1 class="mt-10 mb-5 text-center">Simulador de Dados Tabulados</h1>
                                 <div class="mt-10 flex justify-center">
                                     <img src="logo.jpg" width="80px">
                                 </div>
