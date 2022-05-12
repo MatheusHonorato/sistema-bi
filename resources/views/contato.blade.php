acd4722cf9cf8577f87547cf410ff83d640f82f5
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2022-05-12 19:04:32 -0300

    up

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -149,4 +149,23 @@
         </div>
     </div>
 </div>
+<script>
+  function mascaraDeTelefone(telefone){
+    const textoAtual = telefone.value;
+    const isCelular = textoAtual.length === 9;
+    let textoAjustado;
+    
+    if(isCelular) {
+        const parte1 = textoAtual.slice(0,5);
+        const parte2 = textoAtual.slice(5,9);
+        textoAjustado = `${parte1}-${parte2}`        
+    } else {
+        const parte1 = textoAtual.slice(0,4);
+        const parte2 = textoAtual.slice(4,8);
+        textoAjustado = `${parte1}-${parte2}`
+    }
+
+    telefone.value = textoAjustado;
+  }
+</script>
 </x-app-layout>
